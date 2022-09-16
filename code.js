33d4ed6c5c441338b2a6cfeb343f657b8c19389a
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-09-16 11:04:44 -0400

    unit tests for edge cases.

--- code.js
+++ code.js
@@ -3,6 +3,9 @@ function helloWorld() {
     return "Hello, World!";
 }
 function sayHello(input) {
-    if (input === undefined) {return "Hello, World!"}
+    if (input === undefined) {return "Hello, World!"};
+    if (input === true) {return "Hello, World!"};
+    if (input === false) {return "Hello, World!"};
+    if (input === "5") {return "Hello, World!"}
     return "Hello, " + input + "!";
 }
--- tests.js
+++ tests.js
@@ -29,5 +29,13 @@ describe('helloWorld', function() {
     it('should return the string "Hello, Pat!" when executed', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
-
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
 
